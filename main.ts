cf2a5cc08acaa3b4851f7a88a9a8348b7c8ed00b
NG CHING YI <cyng93+github@gmail.com>
2021-04-09 22:51:41 +0800

    Broadcast to luckydraw receiver

--- main.ts
+++ main.ts
@@ -3,22 +3,26 @@ let threshold = 8
 let distances = 100
 let prev_distances = 100
 basic.forever(function () {
+    radio.setGroup(99)
     distances = sonar.ping(
     DigitalPin.P0,
     DigitalPin.P1,
     PingUnit.Centimeters
     )
     serial.writeLine("distances: " + convertToText(distances))
-    basic.pause(500)
+    basic.pause(100)
+    prev_distances = distances
 })
 basic.forever(function () {
     if (distances < threshold && prev_distances < threshold) {
         pins.digitalWritePin(DigitalPin.P9, 1)
         pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showArrow(ArrowNames.North)
         soundExpression.hello.play()
+        radio.sendString("go")
+        basic.clearScreen()
     } else {
         pins.digitalWritePin(DigitalPin.P9, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
     }
-    prev_distances = distances
 })
