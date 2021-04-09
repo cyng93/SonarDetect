e40def24ec33f55d1f21bbddc9e4fd3704cb88e3
NG CHING YI <cyng93+github@gmail.com>
2021-04-10 00:08:49 +0800

    Add Countdown feature

--- main.ts
+++ main.ts
@@ -1,28 +1,51 @@
-music.setVolume(100)
+function 廣播開始抽獎 () {
+    if (input.runningTime() - prev_trigger > cooldown_ms) {
+        basic.showArrow(ArrowNames.North)
+        radio.sendString("go")
+    } else {
+        music.stopAllSounds()
+        soundExpression.sad.play()
+    }
+    prev_trigger = input.runningTime()
+    basic.pause(100)
+    basic.clearScreen()
+}
+let prev_trigger = 0
+let cooldown_ms = 0
+cooldown_ms = 6000
+prev_trigger = 0 - cooldown_ms
 let threshold = 8
-let distances = 100
-let prev_distances = 100
+let distance = 100
+let prev_distance = 100
 basic.forever(function () {
     radio.setGroup(99)
-    distances = sonar.ping(
+    distance = sonar.ping(
     DigitalPin.P0,
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    serial.writeLine("distances: " + convertToText(distances))
-    basic.pause(100)
-    prev_distances = distances
+    serial.writeLine("distances: " + convertToText(distance))
+    basic.pause(200)
+    prev_distance = distance
 })
 basic.forever(function () {
-    if (distances < threshold && prev_distances < threshold) {
+    if (distance < threshold && prev_distance < threshold) {
         pins.digitalWritePin(DigitalPin.P9, 1)
         pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.showArrow(ArrowNames.North)
         soundExpression.hello.play()
-        radio.sendString("go")
-        basic.clearScreen()
+        廣播開始抽獎()
     } else {
-        pins.digitalWritePin(DigitalPin.P9, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P9, 0)
+    }
+})
+basic.forever(function () {
+    if (input.runningTime() - prev_trigger > cooldown_ms) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        led.plotBarGraph(
+        cooldown_ms - (input.runningTime() - prev_trigger),
+        cooldown_ms
+        )
     }
 })
